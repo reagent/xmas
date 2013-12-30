34c67ebe1b395f21654e0aab5a004e2ebe5e2391
Patrick Reagan <reaganpr@gmail.com>
2013-12-29 23:04:34 -0700

    Trigger music when button is pushed

--- xmas.ino
+++ xmas.ino
@@ -1,11 +1,14 @@
 #include "Player.h"
 
-const short SPEAKER_PIN = 9;  // PWM PIN speaker is connected to
+const short SPEAKER_PIN = 9; // PWM PIN speaker is connected to
+const short BUTTON_PIN = 2;  // PIN pushbutton is connected to
 
 Player player(SPEAKER_PIN);
 
 void setup() {
     Serial.begin(9600);
+
+    pinMode(BUTTON_PIN, INPUT);
 
     player.addInstructions("G4:4,C5:4,C5:2,D5:2,C5:2,B4:2,A4:4,A4:4");
     player.addInstructions("A4:4,D5:4,D5:2,E5:2,D5:2,C5:2,B4:4,G4:4");
@@ -13,7 +16,7 @@ void setup() {
 }
 
 void loop() {
-    player.play();
-
-    while(true) {} // Play once
+    if (digitalRead(BUTTON_PIN) == LOW) {
+       player.play();
+    }
 }
